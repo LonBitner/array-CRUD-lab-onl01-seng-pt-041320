06a30e2f18a94199e40b6390502e7cd280cf695d
Lon Bitner <lonbitner@gmail.com>
2020-04-17 19:44:56 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,7 +13,7 @@ end
 
 def add_element_to_start_of_array(array, element)
  dogs = ["dog"]
-  dogs .#unshift "arrays!" 
+  dogs .unshift "arrays!" 
 end
 
 def remove_element_from_end_of_array(array)
