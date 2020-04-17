0c5086eab7b84b46a6ec58b2d67822d952aa8ff6
Lon Bitner <lonbitner@gmail.com>
2020-04-17 19:54:20 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,12 +27,13 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  dogs = ["dog", "lab", "am"]
+  dogs = ["wow", "lab", "am"]
   dogs[2]
 end
 
 def retrieve_first_element_from_array(array)
-  
+   dogs = ["wow", "lab", "am"]
+  dogs[0] 
 end
 
 def retrieve_last_element_from_array(array)
